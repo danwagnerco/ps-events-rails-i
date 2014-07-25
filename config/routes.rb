f44ce29afc5c38ad3e8223a3b104fce4f1c211df
Dan Wagner <danwagnerco@gmail.com>
2014-07-25 06:48:10 -0500

    finish day 27 nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Events::Application.routes.draw do
-  resources :registrations
 
+  resources :events do
+    resources :registrations
+  end
   root "events#index"
   # get "events"     => "events#index"
   # get "events/:id" => "events#show", as: "event"
   # get "events/:id/edit" => "events#edit", as: "edit_event"
   # patch "events/:id" => "events#update"
-  resources :events
 end
